a04f90dbb8ed4e423668b77c4f38efc96e7f0b92
nitromelon <112331897+nitromelon@users.noreply.github.com>
2026-09-01 21:28:37 +0700

    fix workflow

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === "production" ? process.env.BASE_PATH : "",
+        },
+    },
     compilerOptions: {
         cssHash: ({ hash, css }) => `t${hash(css).substring(1, 1)}`,
     },
