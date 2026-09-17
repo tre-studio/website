89cfbc7c6af7b954716a7dddbfd69476d82e2f58
nitromelon <112331897+nitromelon@users.noreply.github.com>
2026-09-17 13:31:04 +0700

    minor fixes

---
+++ src/routes/(pages)/games/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+    <title>Tre Studio | Games</title>
+</svelte:head>
+
+<h1>
+    <pre>
+     88
+    .8'
+   .8'   .oooooooo  .oooo.   ooo. .oo.  .oo.    .ooooo.   .oooo.o
+  .8'   888' `88b  `P  )88b  `888P"Y88bP"Y88b  d88' `88b d88(  "8
+ .8'    888   888   .oP"888   888   888   888  888ooo888 `"Y88b.
+.8'     `88bod8P'  d8(  888   888   888   888  888    .o o.  )88b
+88      `8oooooo.  `Y888""8o o888o o888o o888o `Y8bod8P' 8""888P'
+        d"     YD
+        "Y88888P'</pre>
+</h1>
+
+<p class="intro">Games</p>
+
+<style>
+    h1 {
+        margin-top: 4em;
+        margin-bottom: 3em;
+    }
+
+    p,
+    .intro {
+        font-size: 1.25em;
+    }
+</style>
